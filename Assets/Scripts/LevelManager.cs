2802a1567e4a19cdb4aa278ab08055197538bdd8
Diogo Rodrigues <diogofgr@gmail.com>
2017-06-30 13:15:18 +0100

    Fade scene with cube instead of UI element

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -6,7 +6,8 @@ using UnityEngine.UI;
 
 public class LevelManager : MonoBehaviour {
 
-	public Image black;
+//	public Image black;
+	public GameObject sceneFader;
 	public Animator animator;
 
 	public void LoadLevel(string name){
@@ -26,12 +27,16 @@ public class LevelManager : MonoBehaviour {
 
 	IEnumerator Fading(){
 		animator.SetBool ("Fade", true);
+		SpriteRenderer black = sceneFader.GetComponent<SpriteRenderer>();
+//		yield return new WaitUntil (() => black.color == new Color32 (255, 255, 255, 1));
 		yield return new WaitUntil (() => black.color.a == 1);
+//		print ("sprite color: " + black.color.a);
 		SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex + 1);
 	}
 
 	IEnumerator FadingToScene(string name){
 		animator.SetBool ("Fade", true);
+		SpriteRenderer black = sceneFader.GetComponent<SpriteRenderer>();
 		yield return new WaitUntil (() => black.color.a == 1);
 		SceneManager.LoadScene(name);
 	}
